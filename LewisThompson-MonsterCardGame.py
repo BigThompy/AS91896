1539651eff9f4d0d03416b3ecbac5ad1c19ee2d5
SpacemanGo15 <lewisedward110@gmail.com>
2021-05-10 09:52:14 +1200

    Defining monster cards and laying out the code

---
+++ LewisThompson-MonsterCardGame.py
@@ -0,0 +1,32 @@
+
+import easygui
+
+
+# The template for existing monster and creating new monsters
+MonsterTemplate = {"Name": 0, "Strength": 0, "Speed":0, "Stealth": 0, "Cunning": 0}
+
+# All existing monster cards
+Stoneling = {"Name": 'Stoneling', "Strength": 7, "Speed":1, "Stealth": 25, "Cunning": 15}
+Vexscream = {"Name": 'Vexscream', "Strength": 1, "Speed":6, "Stealth": 21, "Cunning": 19}
+Dawnmirage = {"Name": 'Dawnmirage', "Strength":5, "Speed":15, "Stealth": 18, "Cunning": 22}
+Blazegolem = {"Name": 'Blazegolem', "Strength": 15, "Speed":20, "Stealth": 23, "Cunning":6}
+Websnake = {"Name": 'Websnake', "Strength": 7, "Speed":15, "Stealth": 10, "Cunning": 5}
+Moldvine = {"Name": 'Moldvine', "Strength": 21, "Speed":18, "Stealth": 14, "Cunning": 5}
+Vortexwing= {"Name": 'Vortexwing', "Strength": 19, "Speed":13, "Stealth": 19, "Cunning": 2}
+Rotthing= {"Name": 'Rotthing', "Strength": 16, "Speed":7, "Stealth": 4, "Cunning": 12}
+Froststep = {"Name": 'Froststep', "Strength": 14, "Speed":14, "Stealth": 17, "Cunning": 4}
+Wispghoul = {"Name": 'Wispghoul', "Strength": 17, "Speed":19, "Stealth": 3, "Cunning": 2}
+
+# All monsters, user cards will be added to this also
+FullCatalogue = [Stoneling, Vexscream, Dawnmirage, Blazegolem, Websnake, Moldvine, Vortexwing, Rotthing, Froststep, Wispghoul]
+
+# Main menu function
+
+
+# Adding new monster cards to the catalogue
+
+
+# Deleting a monster from the catalogue
+
+
+# Outputing monster to shell
