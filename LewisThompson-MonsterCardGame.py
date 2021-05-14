2ce651ac1516db25d51b89b20eee1b8959a22e39
SpacemanGo15 <lewisedward110@gmail.com>
2021-05-14 14:31:34 +1200

    Added Pickle file saving and loading

--- LewisThompson-MonsterCardGame.py
+++ LewisThompson-MonsterCardGame.py
@@ -24,6 +24,32 @@ FullCatalogue = {
                 'Wispghoul': {"Name": 'Wispghoul', "Strength": 17, "Speed":19, "Stealth": 3, "Cunning": 2}
 }
 
+# Saving the monsters to a file
+def saveData():
+    global FullCatalogue
+    with (open("monsters.monster", 'wb')) as save:
+        pickle.dump(FullCatalogue,save)
+
+# Loading the monsters
+def loadData():
+    global FullCatalogue
+    try:
+        with (open("monsters.monster", 'rb')) as save:
+            FullCatalogue = pickle.load(save)
+    except FileNotFoundError:
+        FullCatalogue = {
+                'Stoneling': {"Name": 'Stoneling', "Strength": 7, "Speed":1, "Stealth": 25, "Cunning": 15},
+                'Vexscream': {"Name": 'Vexscream', "Strength": 1, "Speed":6, "Stealth": 21, "Cunning": 19},
+                'Dawnmirage': {"Name": 'Dawnmirage', "Strength":5, "Speed":15, "Stealth": 18, "Cunning": 22},
+                'Blazegolem': {"Name": 'Blazegolem', "Strength": 15, "Speed":20, "Stealth": 23, "Cunning":6},
+                'Websnake': {"Name": 'Websnake', "Strength": 7, "Speed":15, "Stealth": 10, "Cunning": 5},
+                'Moldvine': {"Name": 'Moldvine', "Strength": 21, "Speed":18, "Stealth": 14, "Cunning": 5},
+                'Vortexwing': {"Name": 'Vortexwing', "Strength": 19, "Speed":13, "Stealth": 19, "Cunning": 2},
+                'Rotthing': {"Name": 'Rotthing', "Strength": 16, "Speed":7, "Stealth": 4, "Cunning": 12},
+                'Froststep': {"Name": 'Froststep', "Strength": 14, "Speed":14, "Stealth": 17, "Cunning": 4},
+                'Wispghoul': {"Name": 'Wispghoul', "Strength": 17, "Speed":19, "Stealth": 3, "Cunning": 2}
+}
+
 
 # Adding new monster cards to the catalogue
 def addMonster():
